c99dc0a8ebc9e998f5170b629ac44ef565f5415d
Sushant Kumar <sushant.k@planktonsolutions.com>
2018-05-19 18:52:22 +0530

    sanitize client input

--- ugc_serv/handler/loginUser.handler.js
+++ ugc_serv/handler/loginUser.handler.js
@@ -6,6 +6,7 @@ exports.handler = function (req, res, qpaths, qdata) {
     var moment = require('moment');
     var md5 = require('md5');
     var bearer = require("../function/bearer.func");
+    var sanitizeJson = require("../function/sanitizeJson.func");
 
     var reqBodyStr = '';
     var reqBodyObj = {};
@@ -13,50 +14,56 @@ exports.handler = function (req, res, qpaths, qdata) {
         reqBodyStr += data;
     });
     req.on('end', function () {
-        reqBodyObj = JSON.parse(reqBodyStr);
-        var sql = "SELECT COUNT(*) AS COUNT FROM user_credentials WHERE username = '" + reqBodyObj.username + "' AND password_hash = '" + md5(reqBodyObj.password) + "'";
-        db_conn.query(sql, function (err, result, fields) {
-            if (err) {
-                logger.error(err);
-                res.statusCode = 500;
-                res.statusMessage = "Internal Server Error";
-                res.end();
-            } else {
-                switch (result[0].COUNT) {
-                    case 0:
-                        res.statusCode = 403;
-                        res.statusMessage = "Forbidden";
-                        res.end();
-                        break;
-                    case 1:
-                        var token = md5(moment(new Date()).format("YYYYMMDD_kkmmss") + "__" + unique());
-                        var sql = "DELETE FROM user_token WHERE username = '" + reqBodyObj.username + "'; " +
-                            "INSERT INTO user_token (token, username) VALUES ('" + token + "', '" + reqBodyObj.username + "'); " +
-                            "INSERT INTO login_log (username, login_token, using_token) VALUES ('" + reqBodyObj.username +"', '" + token + "', 0);";
-                        db_conn.query(sql, function (err, result, fields) {
-                            if (err) {
-                                logger.error(err);
-                                res.statusCode = 500;
-                                res.statusMessage = "Internal Server Error";
-                                res.end();
-                            } else {
-                                res.write(JSON.stringify({
-                                    token: token
-                                }));
-                                res.statusCode = 200;
-                                res.statusMessage = "OK";
-                                res.end();
-                            }
-                        });
-                        break;
-                    default:
-                        logger.error(err);
-                        res.statusCode = 500;
-                        res.statusMessage = "Internal Server Error";
-                        res.end();
-                        break;
+        reqBodyObj = sanitizeJson(reqBodyStr, ["username", "password"]);
+        if (reqBodyObj.username && reqBodyObj.password) {
+            var sql = "SELECT COUNT(*) AS COUNT FROM user_credentials WHERE username = '" + reqBodyObj.username + "' AND password_hash = '" + md5(reqBodyObj.password) + "'";
+            db_conn.query(sql, function (err, result, fields) {
+                if (err) {
+                    logger.error(err);
+                    res.statusCode = 500;
+                    res.statusMessage = "Internal Server Error";
+                    res.end();
+                } else {
+                    switch (result[0].COUNT) {
+                        case 0:
+                            res.statusCode = 403;
+                            res.statusMessage = "Forbidden";
+                            res.end();
+                            break;
+                        case 1:
+                            var token = md5(moment(new Date()).format("YYYYMMDD_kkmmss") + "__" + unique());
+                            var sql = "DELETE FROM user_token WHERE username = '" + reqBodyObj.username + "'; " +
+                                "INSERT INTO user_token (token, username) VALUES ('" + token + "', '" + reqBodyObj.username + "'); " +
+                                "INSERT INTO login_log (username, login_token, using_token) VALUES ('" + reqBodyObj.username + "', '" + token + "', 0);";
+                            db_conn.query(sql, function (err, result, fields) {
+                                if (err) {
+                                    logger.error(err);
+                                    res.statusCode = 500;
+                                    res.statusMessage = "Internal Server Error";
+                                    res.end();
+                                } else {
+                                    res.write(JSON.stringify({
+                                        token: token
+                                    }));
+                                    res.statusCode = 200;
+                                    res.statusMessage = "OK";
+                                    res.end();
+                                }
+                            });
+                            break;
+                        default:
+                            logger.error(err);
+                            res.statusCode = 500;
+                            res.statusMessage = "Internal Server Error";
+                            res.end();
+                            break;
+                    }
                 }
-            }
-        });
+            });
+        } else {
+            res.statusCode = 400;
+            res.statusMessage = "Bad Request";
+            res.end();
+        }
     });
 };
