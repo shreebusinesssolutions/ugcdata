b97e624d4cfff13d29380376dc7698ebc7dae2cf
Sushant Kumar <sushant.kum96@gmail.com>
2018-05-22 18:17:34 +0530

    ...

--- data_uploader/college_details.js
+++ data_uploader/college_details.js
@@ -41,10 +41,10 @@ db_conn.connect(function (err) {
                     var college_id = college_data[i][0]
                     db_conn.query(sql, function (err, result, fields) {
                         if (err) {
-                            logger.error(err);
+                            logger.error(err, result);
                             failed++;
                         } else {
-                            logger.log("Query OK");
+                            logger.log("Query OK", result);
                             passed++;
                         }
                         if (i == college_data.length)
