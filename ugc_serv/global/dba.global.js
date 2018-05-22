bf57db0da8ff7459d5eb65638042ed1199cd969f
Sushant Kumar <sushant.k@planktonsolutions.com>
2018-05-22 11:48:49 +0530

    set heartbeat_timeout to 1 hr

--- ugc_serv/global/dba.global.js
+++ ugc_serv/global/dba.global.js
@@ -2,4 +2,4 @@ exports.servername = "ugcdata.tk";
 exports.username = "ugcdata";
 exports.password = "u6cd@t@";
 exports.dbname = "ugcdata";
-exports.heartbeat_interval = 3600;
+exports.heartbeat_interval = 3600000;
