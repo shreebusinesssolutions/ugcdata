90e7182ace58211ab7c6f8b67944d1fbbfb7714b
Sushant Kumar <sushant.kum96@gmail.com>
2018-05-23 13:06:44 +0530

    ...

--- data_uploader/old_coll_id.js
+++ data_uploader/old_coll_id.js
@@ -15,7 +15,7 @@ db_conn.connect(function (err) {
     if (err)
         logger.error(err);
     else {
-        https.get('https://sheets.googleapis.com/v4/spreadsheets/1mnG6XmP72kldNbl-k58BCnYkq9E5OwFKH1qGvn198xU/values/XIANDXIIPLAN!A2:O100?key=AIzaSyDw7EtIbTGxIk3aXtZWgt0bnNcsHlB2yG8', (resp) => {
+        https.get('https://sheets.googleapis.com/v4/spreadsheets/1mnG6XmP72kldNbl-k58BCnYkq9E5OwFKH1qGvn198xU/values/XIANDXIIPLAN!A2:O?key=AIzaSyDw7EtIbTGxIk3aXtZWgt0bnNcsHlB2yG8', (resp) => {
             let data = '';
 
             resp.on('data', (chunk) => {
@@ -23,23 +23,23 @@ db_conn.connect(function (err) {
             });
 
             resp.on('end', () => {
-                console.log(data);
+                var data_obj = JSON.parse(data).values;
+                console.log(data_obj.length);
                 // var sql = "";
                 // var passed = 0, failed = 0;
-                // var college_data = JSON.parse(data).values;
-                // for (var i = 0; i < college_data.length; i++) {
+                // for (var i = 0; i < data_obj.length; i++) {
                 //     sql = "INSERT INTO college (college_id, college_name, addr1, addr2, pin, pfms_unique_code, naac_validity, bsr_intrest_paid_and_intrest) ";
                 //     sql += "VALUES (";
-                //     sql += "'" + college_data[i][0].replace(/'/g, "\\'") + "', ";
-                //     sql += "'" + college_data[i][1].replace(/'/g, "\\'") + "', ";
-                //     sql += "'" + college_data[i][2].replace(/'/g, "\\'") + "', ";
-                //     sql += (college_data[i][3] ? ("'" + college_data[i][3].replace(/'/g, "\\'") + "', ") : "null, ");
-                //     sql += (college_data[i][4] ? ("" + college_data[i][4].replace(/\s/g, "") + ", ") : "null, ");
-                //     sql += (college_data[i][5] ? ("'" + college_data[i][5].replace(/'/g, "\\'") + "', ") : "null, ");
-                //     sql += (college_data[i][6] ? ("'" + moment(college_data[i][6]).format("YYYY-MM-DD") + "', ") : "null, ");
-                //     sql += (college_data[i][7] ? ("'" + college_data[i][7].replace(/'/g, "\\'") + "'") : "null");
+                //     sql += "'" + data_obj[i][0].replace(/'/g, "\\'") + "', ";
+                //     sql += "'" + data_obj[i][1].replace(/'/g, "\\'") + "', ";
+                //     sql += "'" + data_obj[i][2].replace(/'/g, "\\'") + "', ";
+                //     sql += (data_obj[i][3] ? ("'" + data_obj[i][3].replace(/'/g, "\\'") + "', ") : "null, ");
+                //     sql += (data_obj[i][4] ? ("" + data_obj[i][4].replace(/\s/g, "") + ", ") : "null, ");
+                //     sql += (data_obj[i][5] ? ("'" + data_obj[i][5].replace(/'/g, "\\'") + "', ") : "null, ");
+                //     sql += (data_obj[i][6] ? ("'" + moment(data_obj[i][6]).format("YYYY-MM-DD") + "', ") : "null, ");
+                //     sql += (data_obj[i][7] ? ("'" + data_obj[i][7].replace(/'/g, "\\'") + "'") : "null");
                 //     sql += "); ";
-                //     var college_id = college_data[i][0]
+                //     var college_id = data_obj[i][0]
                 //     db_conn.query(sql, function (err, result, fields) {
                 //         if (err) {
                 //             logger.error(err, result);
