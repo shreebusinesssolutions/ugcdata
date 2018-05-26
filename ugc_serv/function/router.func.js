73ffcc00a75536ee88942ac459f743e2dd0b5354
Sushant Kumar <sushant.kum96@gmail.com>
2018-05-26 14:20:07 +0530

    added /data/paid/subscheme/

--- ugc_serv/function/router.func.js
+++ ugc_serv/function/router.func.js
@@ -7,6 +7,7 @@ var _getPaidYear = require("../handler/getPaidYear.handler");
 var _getPaidPaid = require("../handler/getPaidPaid.handler");
 var _getPaidUC = require("../handler/getPaidUC.handler");
 var _getPaidScheme = require("../handler/getPaidScheme.handler");
+var _getPaidSubScheme = require("../handler/getPaidSubScheme.handler");
 var logger = require("../function/logger.func");
 
 exports.route = function (method, pathname) {
@@ -28,6 +29,8 @@ exports.route = function (method, pathname) {
         return _getPaidUC;
     else if (method == "GET" && /^\/data\/paid\/scheme\//.test(pathname))
         return _getPaidScheme;
+    else if (method == "GET" && /^\/data\/paid\/subscheme\//.test(pathname))
+        return _getPaidSubScheme;
     else {
         logger.error(pathname + " (" + method + ") Not found.", { method: "router()" });
         return false;
---
+++ ugc_serv/handler/getPaidSubScheme.handler.js
@@ -0,0 +1,56 @@
+exports.handler = function (req, res, qpaths, qdata) {
+    var unique = require('uniqid');
+    var moment = require('moment-timezone');
+    var md5 = require('md5');
+    tokenUsed = require("../function/dba_func/tokenUsed.dba_func");
+    var sanitizeJson = require("../function/sanitizeJson.func");
+    var bearer = require("../function/bearer.func");
+
+    bearer.getToken(req, function (err, token) {
+        if (err) {
+            logger.error(err);
+            res.statusCode = 500;
+            res.statusMessage = "Internal Server Error";
+            res.end();
+        }
+        else {
+            bearer.verify(token, req.headers["username"], function (err) {
+                if (err) {
+                    logger.error(err, { username: req.headers["username"] });
+                    if (err.message == "bearer_auth_error") {
+                        res.statusCode = 403;
+                        res.statusMessage = "Forbidden";
+                        res.end();
+                    }
+                    else {
+                        res.statusCode = 500;
+                        res.statusMessage = "Internal Server Error";
+                        res.end();
+                    }
+                }
+                else {
+                    var sql = "SELECT subscheme_id, subscheme_name FROM sub_scheme ORDER BY scheme_id asc";
+                    db_conn.query(sql, function (err, result, fields) {
+                        if (err) {
+                            logger.error(err);
+                            res.statusCode = 500;
+                            res.statusMessage = "Internal Server Error";
+                            res.end();
+                        } else {
+                            var responseObj = [];
+                            for (var i = 0; i < result.length; i++)
+                                responseObj.push({
+                                    id: result[i].subscheme_id,
+                                    name: result[i].subscheme_name
+                                });
+                            res.write(JSON.stringify(responseObj));
+                            res.statusCode = 200;
+                            res.statusMessage = "OK";
+                            res.end();
+                        }
+                    })
+                }
+            })
+        }
+    });
+};
