f9e7c69549db5458eb2b0cf3159e237fe01360cf
Sushant Kumar <sushant.kum96@gmail.com>
2018-05-29 18:43:08 +0530

    starting pending.html

---
+++ www/ugcdata.tk/js/pending-ng.js
@@ -0,0 +1,9 @@
+const info = {
+    page: "pending.html",
+    path: "/report/pending/"
+};
+Object.freeze(info);
+
+$(window).ready(function () {
+    scope = angular.element(document.getElementById("ng-app")).scope();
+});
