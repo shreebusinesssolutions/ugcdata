52b9ac875e37cb0e1ba5c128783299fa9a8805ff
Sushant Kumar <sushant.kum96@gmail.com>
2018-05-31 14:29:55 +0530

    added uc filter

--- www/ugcdata.tk/js/pending-ng.js
+++ www/ugcdata.tk/js/pending-ng.js
@@ -72,12 +72,13 @@ var PendingCtrl = (function () {
                 max: null,
                 includeBlanks: false
             },
-            // uc: {
-            //     scaleMin: null,
-            //     scaleMax: null,
-            //     min: null,
-            //     max: null
-            // },
+            uc: {
+                scaleMin: null,
+                scaleMax: null,
+                min: null,
+                max: null,
+                includeBlanks: false
+            },
             // year: {
             //     selected: [],
             //     selectedItem: null,
@@ -209,19 +210,19 @@ var PendingCtrl = (function () {
                 _this.httpResponseError(response);
             });
         }, 100);
-        // this.$timeout(function () {
-        //     _this.$http({
-        //         method: "GET",
-        //         url: "/ugc_serv/data/paid/uc/"
-        //     }).then(function successCallback(response) {
-        //         _this.filter.uc.scaleMin = response.data.min;
-        //         _this.filter.uc.scaleMax = response.data.max;
-        //         _this.filter.uc.min = response.data.min;
-        //         _this.filter.uc.max = response.data.max;
-        //     }, function errorCallback(response) {
-        //         _this.httpResponseError(response);
-        //     });
-        // }, 100);
+        this.$timeout(function () {
+            _this.$http({
+                method: "GET",
+                url: "/ugc_serv/data/pending/uc/"
+            }).then(function successCallback(response) {
+                _this.filter.uc.scaleMin = response.data.min;
+                _this.filter.uc.scaleMax = response.data.max;
+                _this.filter.uc.min = response.data.min;
+                _this.filter.uc.max = response.data.max;
+            }, function errorCallback(response) {
+                _this.httpResponseError(response);
+            });
+        }, 100);
         // this.$timeout(function () {
         //     _this.$http({
         //         method: "GET",
