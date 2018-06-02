5c87ae3aa01d41ab78d26018f2ffbbad7088386b
Sushant Kumar <sushant.kum96@gmail.com>
2018-06-02 19:26:14 +0530

    added college.html

--- www/ugcdata.tk/js/college-ng.js
+++ www/ugcdata.tk/js/college-ng.js
@@ -1,14 +1,55 @@
 var CollegeCtrl = (function () {
-    function CollegeCtrl($scope, $mdDialog, $mdToast, $timeout) {
+    function CollegeCtrl($scope, $mdDialog, $mdToast, $timeout, $http) {
         this.$scope = $scope;
         this.$mdDialog = $mdDialog;
         this.$mdToast = $mdToast;
         this.$timeout = $timeout;
+        this.$http = $http;
         this.primary = 'blue';
         this.accent = 'red';
         this.white = 'white';
 
-        this.mode = {};
+        this.mode = {
+            report: {
+                getting: false,
+                loaded: false
+            }
+        };
+
+        this.filter = {
+            college: {
+                selected: [],
+                selectedItem: null,
+                search: "",
+                every: null,
+                hasBlanks: false,
+                includeBlanks: false
+            }
+        };
+
+        this.reportData = {
+            totalCount: null,
+            maxChunkSize: 1000,
+            limit: {
+                min: null,
+                max: null
+            },
+            data: []
+        };
+
+        var _this = this;
+        this.$timeout(function () {
+            _this.$http({
+                method: "GET",
+                url: "/ugc_serv/data/global/college/"
+            }).then(function successCallback(response) {
+                _this.filter.college.every = response.data;
+                if (response.data.indexOf("(Blank)") >= 0)
+                    _this.filter.college.hasBlanks = true;
+            }, function errorCallback(response) {
+                _this.httpResponseError(response);
+            });
+        }, 100);
     }
 
     CollegeCtrl.prototype.showNotif = function (message, timeout = 3000, errorToast = false) {
@@ -50,15 +91,253 @@ var CollegeCtrl = (function () {
             }, 50);
         }
     };
-    CollegeCtrl.prototype.openMenu = function($$mdMenu,  $$event) {
+    CollegeCtrl.prototype.showAlert = function (title, content, okText = "OK", confirmCallback = function () { }) {
+        var _this = this;
+        var alert = _this.$mdDialog.alert({
+            title: title,
+            textContent: content,
+            ok: okText,
+            ariaLabel: "Alert Dialog",
+        });
+        _this.$mdDialog.show(alert).then(function () {
+            confirmCallback();
+        })
+    };
+    CollegeCtrl.prototype.showConfirm = function (title, content, okText = "OK", cancelText = "Cancel", confirmCallback = function () { }, cancelCallback = function () { }) {
+        var _this = this;
+        var confirm = _this.$mdDialog.confirm({
+            title: title,
+            textContent: content,
+            ariaLabel: "Confirm Dialog",
+            ok: okText,
+            cancel: cancelText
+        });
+        _this.$mdDialog.show(confirm).then(function () {
+            confirmCallback();
+        }, function () {
+            cancelCallback();
+        });
+    };
+    CollegeCtrl.prototype.openMenu = function ($$mdMenu, $$event) {
         $$mdMenu.open($$event);
     };
+    CollegeCtrl.prototype.transformCollegeChip = function (chip) {
+        return chip;
+    };
+    CollegeCtrl.prototype.querySearchCollege = function (query) {
+        var results = query ? this.filter.college.every.filter(createFilterFor(query)) : this.filter.college.every.filter(createFilterFor(''));
+        return results;
+    };
+    CollegeCtrl.prototype.getReport = function (min, max) {
+        var _this = this;
+        _this.mode.report.getting = true;
+        _this.mode.report.loaded = false;
+        _this.$http({
+            method: "GET",
+            url: "/ugc_serv/datatable/college/count/?collegeId=" + decodeURI(_this.filter.college.selected.join(";,;"))
+        }).then(function successCallback(response) {
+            _this.reportData.totalCount = response.data;
+            _this.reportData.limit.min = 0;
+            _this.reportData.limit.max = _this.reportData.maxChunkSize;
+            if (_this.reportData.totalCount > _this.reportData.maxChunkSize) {
+                _this.showConfirm(
+                    "Too much data.",
+                    "The data you tried to fetch has " + _this.reportData.totalCount + " entries. Try to modify your filter to produce more sharp results or the data will be fetched in chunks of " + _this.reportData.maxChunkSize + ". Proceed?",
+                    "Yes (fetch data in chunks)",
+                    "No (Modify filter)",
+                    function () {
+                        _this.$http({
+                            method: "GET",
+                            url: "/ugc_serv/datatable/college/?collegeId=" + decodeURI(_this.filter.college.selected.join(";,;"))
+                        }).then(function successCallback(response) {
+                            if ($.fn.dataTable.isDataTable('#dataTable')) {
+                                console.log("Destrying", dtTable);
+                                dtTable.destroy(true);
+                                document.getElementById("divDataTable").innerHTML = '\
+                                    <table id="dataTable" class="table table-striped table-bordered compact hover order-column" style="width:100%">\
+                                        <thead>\
+                                            <tr>\
+                                                <th>College ID</th>\
+                                                <th>Old College ID</th>\
+                                                <th>Name</th>\
+                                                <th>Address 1</th>\
+                                                <th>Address 2</th>\
+                                                <th>PIN</th>\
+                                                <th>PFMS Unique Code</th>\
+                                                <th>NAAC Validity</th>\
+                                                <th>BSR Int. Paid and Intrest</th>\
+                                            </tr>\
+                                        </thead>\
+                                        <tfoot>\
+                                            <tr>\
+                                                <th>College ID</th>\
+                                                <th>Old College ID</th>\
+                                                <th>Name</th>\
+                                                <th>Address 1</th>\
+                                                <th>Address 2</th>\
+                                                <th>PIN</th>\
+                                                <th>PFMS Unique Code</th>\
+                                                <th>NAAC Validity</th>\
+                                                <th>BSR Int. Paid and Intrest</th>\
+                                            </tr>\
+                                        </tfoot>\
+                                    </table>';
+                            }
+                            _this.$timeout(function () {
+                                dtTable = $('#dataTable').DataTable({
+                                    "deferRender": true,
+                                    "lengthMenu": [50, 100],
+                                    "destroy": true
+                                });
+                                console.log(response.data.data.length);
+                                if (response.data.data.length == 0)
+                                    _this.showAlert("No more data", "No more data to fetch.");
+                                for (var i = 0; i < response.data.data.length; i++) {
+                                    dtTable.row.add(response.data.data[i]).draw(false);
+                                }
+                                _this.mode.report.getting = false;
+                                _this.mode.report.loaded = true;
+                            }, 1000);
+                        }, function errorCallback(response) {
+                            _this.mode.report.getting = false;
+                            _this.mode.report.loaded = false;
+                            _this.httpResponseError(response);
+                        });
+                    },
+                    function () {
+                        _this.mode.report.getting = false;
+                        _this.mode.report.loaded = false;
+                        if (document.getElementById("filter_section").classList.contains("w3-hide"))
+                            toggleAccordian("filter", "filter_section");
+                    }
+                );
+            }
+            else {
+                _this.mode.report.getting = true;
+                _this.mode.report.loaded = false;
+                _this.$http({
+                    method: "GET",
+                    url: "/ugc_serv/datatable/college/?collegeId=" + decodeURI(_this.filter.college.selected.join(";,;")) 
+                }).then(function successCallback(response) {
+                    if ($.fn.dataTable.isDataTable('#dataTable')) {
+                        console.log("Destrying", dtTable);
+                        dtTable.destroy(true);
+                        document.getElementById("divDataTable").innerHTML = '\
+                            <table id="dataTable" class="table table-striped table-bordered compact hover order-column" style="width:100%">\
+                                <thead>\
+                                    <tr>\
+                                        <th>College ID</th>\
+                                        <th>Old College ID</th>\
+                                        <th>Name</th>\
+                                        <th>Address 1</th>\
+                                        <th>Address 2</th>\
+                                        <th>PIN</th>\
+                                        <th>PFMS Unique Code</th>\
+                                        <th>NAAC Validity</th>\
+                                        <th>BSR Int. Paid and Intrest</th>\
+                                    </tr>\
+                                </thead>\
+                                <tfoot>\
+                                    <tr>\
+                                        <th>College ID</th>\
+                                        <th>Old College ID</th>\
+                                        <th>Name</th>\
+                                        <th>Address 1</th>\
+                                        <th>Address 2</th>\
+                                        <th>PIN</th>\
+                                        <th>PFMS Unique Code</th>\
+                                        <th>NAAC Validity</th>\
+                                        <th>BSR Int. Paid and Intrest</th>\
+                                    </tr>\
+                                </tfoot>\
+                            </table>';
+                    }
+                    _this.$timeout(function () {
+                        dtTable = $('#dataTable').DataTable({
+                            "deferRender": true,
+                            "lengthMenu": [50],
+                            "destroy": true,
+                        });
+                        console.log(response.data.data.length);
+                        for (var i = 0; i < response.data.data.length; i++) {
+                            dtTable.row.add(response.data.data[i]).draw(false);
+                        }
+                        _this.mode.report.getting = false;
+                        _this.mode.report.loaded = true;
+                    }, 1000);
+                }, function errorCallback(response) {
+                    _this.mode.report.getting = false;
+                    _this.mode.report.loaded = false;
+                    _this.httpResponseError(response);
+                });
+            }
 
+        }, function errorCallback(response) {
+            _this.httpResponseError(response);
+        });
+    };
+    CollegeCtrl.prototype.loadMore = function () {
+        console.log("hi");
+        var _this = scope.vm;
+        _this.mode.report.getting = true;
+        _this.mode.report.loaded = true;
+        _this.reportData.limit.min += _this.reportData.maxChunkSize;
+        _this.reportData.limit.max += _this.reportData.maxChunkSize;
+        console.log(_this.reportData);
+        _this.$http({
+            method: "GET",
+            url: "/ugc_serv/datatable/college/?collegeId=" + decodeURI(_this.filter.college.selected.join(";,;")) 
+        }).then(function successCallback(response) {
+            console.log(response.data.data.length);
+            if (response.data.data.length == 0)
+                _this.showAlert("No more data", "No more data to fetch.");
+            for (var i = 0; i < response.data.data.length; i++) {
+                dtTable.row.add(response.data.data[i]).draw(false);
+            }
+            _this.mode.report.getting = false;
+            _this.mode.report.loaded = true;
+        }, function errorCallback(response) {
+            _this.mode.report.getting = false;
+            _this.mode.report.loaded = true;
+            _this.httpResponseError(response);
+        });
+    };
+
+    function createFilterFor(query) {
+        var lowercaseQuery = angular.lowercase(query);
+
+        return function filterFn(item) {
+            return (item.toLowerCase().indexOf(lowercaseQuery) === 0);
+        };
+    }
+
+    function createFilterObjFor(query) {
+        var lowercaseQuery = angular.lowercase(query);
+
+        return function filterFn(item) {
+            return (item.id.toLowerCase().indexOf(lowercaseQuery) === 0) || (item.name.toLowerCase().indexOf(lowercaseQuery) === 0);
+        };
+    }
+
+    CollegeCtrl.prototype.httpResponseError = function (response) {
+        var _this = this;
+        console.log("error", response);
+        if (response.status == 403) {
+            _this.showNotif("You are not authorized. You'll be redirected in 5 secs.", 3000, true);
+            _this.$timeout(function () {
+                window.location.href = "/";
+            }, 5000);
+        }
+        else {
+            _this.showNotif("Something went wrong. Please try again later.", 3000, true);
+        }
+    }
     CollegeCtrl.$inject = [
         '$scope',
         '$mdDialog',
         '$mdToast',
-        '$timeout'
+        '$timeout',
+        '$http'
     ];
     return CollegeCtrl;
 }());
@@ -78,4 +357,8 @@ angular
             .accentPalette('yellow').dark();
         $mdThemingProvider.theme('dark-primary').backgroundPalette('blue').dark();
         $mdThemingProvider.theme('dark-accent').backgroundPalette('red').dark();
+    })
+    .run(function ($http) {
+        $http.defaults.headers.common.Authorization = 'Bearer ' + cust_localStorage.getItem("token");
+        $http.defaults.headers.common['username'] = cust_localStorage.getItem("username");
     });
