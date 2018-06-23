46e86d77ab086626cd0f60b9bdc5269eeec8dc19
Sushant Kumar <sushant.kum96@gmail.com>
2018-06-23 23:46:58 +0530

    repaired report pending

--- ugc_serv/handler/GET/report/pending/gerReportPending.handler.js
+++ ugc_serv/handler/GET/report/pending/gerReportPending.handler.js
@@ -205,7 +205,7 @@ exports.handler = function (req, res, qpaths, qdata) {
                             else if (!qdata.caseCleared[0] && qdata.caseCleared[1])
                                 sub_query.push("(case_cleared != 1 AND case_cleared =0)")
                             sql += sub_query.join(" " + concat + " ");
-                            sql += "LIMIT " + qdata.limit[0] + ", " + qdata.limit[1]
+                            sql += " LIMIT " + qdata.limit[0] + ", " + qdata.limit[1]
                             
                             db_conn.query(sql, function (err, result, fields) {
                                 if (err) {
